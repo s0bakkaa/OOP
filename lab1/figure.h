2b9e9381f08cb39b8c38c1e459a4f2014c68b087
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-11-12 01:47:34 +0300

    Update figure.h

--- lab1/figure.h
+++ lab1/figure.h
@@ -1,15 +1,15 @@
 #ifndef FIGURE_H
 #define FIGURE_H
 
+#include <ostream>
+
 #include "point.h"
 
-class Figure { 
+class Figure {
 public:
-    virtual size_t VertexesNumber() = 0; //возвращает кол-во вершин фигуры
-    virtual double Area() = 0; //метод расчета площади фигуры
     virtual void Print(std::ostream& os) = 0;
+    virtual double Area() = 0;
     virtual ~Figure() {};
-
 };
 
 #endif // FIGURE_H
