96512271c1eecb86c6baf7f719dd39a4c3265d03
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-11-12 01:52:53 +0300

    Update trapezoid.cpp

--- lab1/trapezoid.cpp
+++ lab1/trapezoid.cpp
@@ -1,45 +1,57 @@
 #include "trapezoid.h"
 
-Trapezoid::Trapezoid(std::istream& is) {
-    std::cout << "Enter the trapezoid's vertexes:" << std::endl;
-    is >> point_a;
-    is >> point_b;
-    is >> point_c;
-    is >> point_d;
-    std::cout << "The trapezoid is created" << std::endl;
+Trapezoid::Trapezoid() : point_a(0.0, 0.0), point_b(0.0, 0.0), point_c(0.0, 0.0), point_d(0.0, 0.0) {
+	std::cout << "Default trapezoid is created" << std::endl;
 }
 
-void Trapezoid::Print(std::ostream& os) {
-    std::cout << "Trapezoid: ";
-    std::cout << point_a << ", ";
-    std::cout << point_b << ", ";
-    std::cout << point_c << ", ";
-    std::cout << point_d << std::endl;
+Trapezoid::Trapezoid(Point a, Point b, Point c, Point d) : point_a(a), point_b(b), point_c(c), point_d(d) {
+	std::cout << "Trapezoid is created with vertices: ";
+	std::cout << point_a << ", ";
+	std::cout << point_b << ", ";
+	std::cout << point_c << ", ";
+	std::cout << point_d << std::endl;
+}
+
+Trapezoid::Trapezoid(std::istream& is) {
+	is >> point_a >> point_b >> point_c >> point_d;
+}
+
+Trapezoid::Trapezoid(const Trapezoid& other) : Trapezoid(other.point_a, other.point_b, other.point_c, other.point_d) {
+	std::cout << "Trapezoid's copy is created" << std::endl;
 }
 
 size_t Trapezoid::VertexesNumber() {
-    size_t number = 4;
-    return number;
+	int vert_num = 4;
+	return vert_num;
 }
 
 double Trapezoid::Area() {
-    try {
-        double len_a = point_b.dist(point_a);
-        double len_b = point_c.dist(point_b);
-        double len_c = point_d.dist(point_c);
-        double len_d = point_a.dist(point_d);
-        if (len_a < 0 || len_b < 0 || len_c < 0 || len_d < 0) {
-            throw "The length of the side is < 0";
-        }
-        double sum = point_a.x_ * point_b.y_ + point_b.x_ * point_c.y_ + point_c.x_ * point_d.y_ + point_d.x_ * point_a.y_;
-        double diff = 0 - point_b.x_ * point_a.y_ - point_c.x_ * point_b.y_ - point_d.x_ * point_c.y_ - point_a.x_ * point_d.y_;
-        return 0.5 * (sum + diff);
-    }
-    catch (const char* exception) {
-        std::cerr << "Error: " << exception << std::endl;
-    }
+	double fig_square = 0.0;
+	double side_a = 0.0;
+	double side_b = 0.0;
+	double height = 0.0;
+	if (point_a.y_ == point_d.y_ && point_b.y_ == point_c.y_) {
+		side_a = point_d.dist(point_a);
+		side_b = point_c.dist(point_b);
+		height = point_b.y_ - point_a.y_;
+	}
+	else {
+		side_a = point_b.dist(point_a);
+		side_b = point_c.dist(point_d);
+		height = point_c.x_ - point_b.x_;
+	}
+	fig_square = 0.5 * (side_a + side_b) * height;
+	return fig_square;
+}
+
+void Trapezoid::Print(std::ostream& os) {
+	os << "Trapezoid: ";
+	os << point_a << " ";
+	os << point_b << " ";
+	os << point_c << " ";
+	os << point_d << std::endl;
 }
 
 Trapezoid::~Trapezoid() {
-    std::cout << "The trapezoid is deleted" << std::endl;
+	std::cout << "Trapezoid is deleted" << std::endl;
 }
