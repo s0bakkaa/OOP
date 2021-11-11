5e2f7c52edbee74afcec536b97cf633802c848e5
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-11-12 01:51:25 +0300

    Update rectangle.h

--- lab1/rectangle.h
+++ lab1/rectangle.h
@@ -1,19 +1,28 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
+#include <iostream>
+
 #include "figure.h"
 
 class Rectangle : public Figure {
 public:
-    Rectangle(std::istream& is);
+	Rectangle();
+	Rectangle(Point a, Point b, Point c, Point d);
+	Rectangle(std::istream& is);
+	Rectangle(const Rectangle& other);
 
-    size_t VertexesNumber();
-    double Area();
-    void Print(std::ostream& os);
-    virtual ~Rectangle();
+	size_t VertexesNumber();
+	double Area();
+	void Print(std::ostream& os);
+
+	virtual ~Rectangle();
 
 private:
-    Point point_a, point_b, point_c, point_d; //  a - low left
+	Point point_a; // lower left corner, then clockwise
+	Point point_b;
+	Point point_c;
+	Point point_d;
 };
 
 #endif // RECTANGLE_H
