da3a06b477836c651befa1eef249e7b9ecd7d981
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-12-01 21:30:01 +0300

    Update point.cpp

--- lab5/point.cpp
+++ lab5/point.cpp
@@ -4,6 +4,10 @@ Point::Point() : x_(0.0), y_(0.0) {}
 
 Point::Point(double x, double y) : x_(x), y_(y) {}
 
+Point::Point(std::istream& is) {
+    is >> x_ >> y_;
+}
+
 double Point::dist(Point& other) {
     double dx = (other.x_ - x_);
     double dy = (other.y_ - y_);
@@ -15,17 +19,11 @@ std::istream& operator>>(std::istream& is, Point& p) {
     return is;
 }
 
-std::ostream& operator<<(std::ostream& os, const Point& p) {
+std::ostream& operator<<(std::ostream& os, Point& p) {
     os << "(" << p.x_ << ", " << p.y_ << ")";
     return os;
 }
 
-Point& Point::operator++() {
-    this->x_ += 1;
-    this->y_ += 1;
-    return *this;
-}
-
-Point operator+(const Point& left, const Point& right) {
-    return Point(left.x_ + right.x_, left.y_ + right.y_);
+Point operator+(Point x, Point y) {
+    return Point(x.x_ + y.x_, x.y_ + y.y_);
 }
