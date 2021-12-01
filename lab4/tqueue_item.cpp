5e007d6a525ca8b0a7c607b786986141d94b6933
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-12-01 19:24:02 +0300

    Update tqueue_item.cpp

--- lab4/tqueue_item.cpp
+++ lab4/tqueue_item.cpp
@@ -1,30 +1,24 @@
-#include <iostream>
 #include "tqueue_item.h"
 
 template <class T>
 TQueueItem<T>::TQueueItem(const std::shared_ptr<T>& square) {
-    this->item = square;
+    this->square = square;
     this->next = nullptr;
     std::cout << "Queue item is created" << std::endl;
 }
 
 template <class T>
 TQueueItem<T>::TQueueItem(const std::shared_ptr<TQueueItem<T>>& other) {
-    this->item = other->item;
+    this->square = other->square;
     this->next = other->next;
-    std::cout << "Queue item: copied" << std::endl;
+    std::cout << "Queue item is copied" << std::endl;
 }
 
 template <class T>
-std::shared_ptr<TQueueItem<T>> TQueueItem<T>::SetNext(std::shared_ptr<TQueueItem<T>>& next_) {
+std::shared_ptr<TQueueItem<T>> TQueueItem<T>::SetNext(std::shared_ptr<TQueueItem<T>> &next) {
     std::shared_ptr<TQueueItem<T>> prev = this->next;
-    this->next = next_;
+    this->next = next;
     return prev;
-}
-
-template <class T>
-std::shared_ptr<T> TQueueItem<T>::GetItem() {
-    return this->item;
 }
 
 template <class T>
@@ -32,12 +26,22 @@ std::shared_ptr<TQueueItem<T>> TQueueItem<T>::GetNext() {
     return this->next;
 }
 
+template <class T>
+std::shared_ptr<T> TQueueItem<T>::GetSquare() const {
+    return this->square;
+}
+
 template <class A>
 std::ostream& operator<<(std::ostream& os, const TQueueItem<A>& obj) {
-    if (obj.item) {
-        os << "{";
-        os << *(obj.item);
-        os << "}" << std::endl;
-    }
+    os << "Item: " << *obj.square << std::endl;
     return os;
 }
+
+template <class T>
+TQueueItem<T>::~TQueueItem() {
+    std::cout << "The queue item is deleted" << std::endl;
+}
+
+#include "square.h"
+template class TQueueItem<Square>;
+template std::ostream& operator<<(std::ostream& os, const TQueueItem<Square>& obj);
