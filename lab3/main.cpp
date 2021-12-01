5958873f62e876e8e49690f2be651bbb9d735ee9
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-12-01 13:35:19 +0300

    Update main.cpp

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,10 +1,31 @@
 #include "tqueue.h"
 
-int main() {
+int main(int argc, char** argv) {
     TQueue queue;
-    std::vector<Point> vect{ Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0) };
-    queue.push(std::make_shared<Square>(Square(vect)));
-    queue.push(std::make_shared<Square>(Square()));
-    std::cout << queue << std::endl;
+    Point a_1(1.0, 1.0);
+    Point b_1(1.0, 2.0);
+    Point c_1(2.0, 2.0);
+    Point d_1(2.0, 1.0);
+    Point a_2(3.0, 1.0);
+    Point b_2(3.0, 3.0);
+    Point c_2(5.0, 3.0);
+    Point d_2(5.0, 1.0);
+    Point a_3(0.0, 0.0);
+    Point b_3(0.0, 4.0);
+    Point c_3(4.0, 4.0);
+    Point d_3(4.0, 0.0);
+    queue.Push(std::shared_ptr<Square>(new Square(a_1, b_1, c_1, d_1)));
+    queue.Push(std::shared_ptr<Square>(new Square(a_2, b_2, c_2, c_2)));
+    queue.Push(std::shared_ptr<Square>(new Square(a_3, b_3, c_3, d_3)));
+    std::cout << queue;
+
+    std::shared_ptr<Square> square;
+    square = queue.Pop();
+    std::cout << *square << std::endl;
+    square = queue.Pop();
+    std::cout << *square << std::endl;
+    square = queue.Pop();
+    std::cout << *square << std::endl;
+
     return 0;
 }
