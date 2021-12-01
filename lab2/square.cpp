9c70c674372afca441ce2c1b8cb4fcfed23408b9
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-12-01 12:15:43 +0300

    Update square.cpp

--- lab2/square.cpp
+++ lab2/square.cpp
@@ -1,86 +1,77 @@
 #include "square.h"
 
+Square::Square() : point_a(0.0, 0.0), point_b(0.0, 0.0), point_c(0.0, 0.0), point_d(0.0, 0.0) {
+	std::cout << "Default square is created" << std::endl;
+}
+
+Square::Square(Point a, Point b, Point c, Point d) : point_a(a), point_b(b), point_c(c), point_d(d) {
+	std::cout << "Square is created with vertices: ";
+	std::cout << point_a << ", ";
+	std::cout << point_b << ", ";
+	std::cout << point_c << ", ";
+	std::cout << point_d << std::endl;
+}
+
+Square::Square(const Square& other) : Square(other.point_a, other.point_b, other.point_c, other.point_d) {
+	std::cout << "Square's copy is created" << std::endl;
+}
+
 double Square::Area() {
-    try {
-        double len_a = point_b.dist(point_a);
-        if (len_a < 0) {
-            throw "The length of the side is < 0";
-        }
-        return pow(len_a, 2);
-    }
-    catch (const char* exception) {
-        std::cerr << "Error: " << exception << std::endl;
-    }
-}
-
-size_t Square::VertexesNumber() {
-    return 4;
-}
-
-Square::Square()
-    : point_a(0.0, 0.0), point_b(0.0, 0.0), point_c(0.0, 0.0), point_d(0.0, 0.0) {
-    std::cout << "Default square is created" << std::endl;
-}
-
-Square::Square(const Square& other) {
-    point_a = other.point_a;
-    point_b = other.point_b;
-    point_c = other.point_c;
-    point_d = other.point_d;
-    std::cout << "Square's copy is created" << std::endl;
-}
-
-Square::Square(std::vector<Point> vect)
-    : point_a(vect[0]), point_b(vect[1]), point_c(vect[2]), point_d(vect[3]) {
-    std::cout << "Square is created with vertexes: " << point_a << ", " << point_b << ", " << point_c << ", " << point_d << std::endl;
+	double side = 0.0;
+	double fig_square = 0.0;
+	side = point_b.dist(point_a);
+	fig_square = side * side;
+	return fig_square;
 }
 
 std::istream& operator>>(std::istream& is, Square& obj) {
-    is >> obj.point_a >> obj.point_b >> obj.point_c >> obj.point_d;
-    return is;
+	is >> obj.point_a >> obj.point_b >> obj.point_c >> obj.point_d;
+	return is;
 }
 
 std::ostream& operator<<(std::ostream& os, const Square& obj) {
-    os << "Point a = " << obj.point_a << ", ";
-    os << "Point b = " << obj.point_b << ", ";
-    os << "Point c = " << obj.point_c << ", ";
-    os << "Point d = " << obj.point_d << std::endl;
-
-    return os;
-}
-
-Square& Square::operator=(const Square& other) {
-    if (this == &other) {
-        return *this;
-    }
-
-    this->point_a = other.point_a;
-    this->point_b = other.point_b;
-    this->point_c = other.point_c;
-    this->point_d = other.point_d;
-
-    std::cout << "Square is copied" << std::endl;
-    return *this;
+	Point a(obj.point_a);
+	Point b(obj.point_b);
+	Point c(obj.point_c);
+	Point d(obj.point_d);
+	os << "Point_a: " << a << ", ";
+	os << "Point_b: " << b << ", ";
+	os << "Point_c: " << c << ", ";
+	os << "Point_d: " << d << std::endl;
+	return os;
 }
 
 Square& Square::operator++() {
-    point_a.x_ += 1.0;
-    point_a.y_ += 1.0;
-    point_b.x_ += 1.0;
-    point_b.y_ += 1.0;
-    point_c.x_ += 1.0;
-    point_c.y_ += 1.0;
-    point_d.x_ += 1.0;
-    point_d.y_ += 1.0;
-    
-    return *this;
+	point_a.x_ += 1.0;
+	point_a.y_ += 1.0;
+	point_b.x_ += 1.0;
+	point_b.y_ += 1.0;
+	point_c.x_ += 1.0;
+	point_c.y_ += 1.0;
+	point_d.x_ += 1.0;
+	point_d.y_ += 1.0;
+
+	return *this;
 }
 
-Square operator+(const Square& l, const Square& r) {
-    std::vector<Point> vect{ l.point_a + r.point_a, l.point_b + r.point_b, l.point_c + r.point_c, l.point_d + r.point_d };
-    return Square(vect);
+Square operator+(const Square& left, const Square& right) {
+	return Square(left.point_a + right.point_a, left.point_b + right.point_b, left.point_c + right.point_c, left.point_d + right.point_d);
+}
+
+Square& Square::operator=(const Square& other) {
+	if (this == &other) {
+		return *this;
+	}
+	else {
+		point_a = other.point_a;
+		point_b = other.point_b;
+		point_c = other.point_c;
+		point_d = other.point_d;
+		std::cout << "Square is copied" << std::endl;
+		return *this;
+	}
 }
 
 Square::~Square() {
-    std::cout << "Square is deleted" << std::endl;
+	std::cout << "Square is deleted" << std::endl;
 }
