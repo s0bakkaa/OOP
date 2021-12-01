88941fbb5358f674c4fa414cb4b3db856b14afbd
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-12-01 21:31:22 +0300

    Update tqueue.cpp

--- lab5/tqueue.cpp
+++ lab5/tqueue.cpp
@@ -1,66 +1,89 @@
-#include<iostream>
 #include "tqueue.h"
 
 template <class T>
-TQueue<T>::TQueue() : first(nullptr), last(nullptr), n(0) {}
+TQueue<T>::TQueue() : head(nullptr), tail(nullptr), num_of_elem(0) {
+
+}
 
 template <class T>
 TQueue<T>::TQueue(const TQueue<T>& other) {
-    first = other.first;
-    last = other.last;
-    n = other.n;
-    std::cout << "Queue is copied" << std::endl;
+    head = other.head;
 }
 
 template <class T>
-void TQueue<T>::push(std::shared_ptr<T>&& item) {
-    std::shared_ptr<TQueueItem<T>> tail =
-        std::make_shared<TQueueItem<T>>(TQueueItem<T>(item));
-    if (this->empty()) {
-        this->first = this->last = tail;
-    }
-    else if (n == 1) {
-        last = tail;
-        first->SetNext(tail);
-    }
-    else {
-        this->last->SetNext(tail);
-        last = tail;
-    }
-    n++;
-}
+std::ostream& operator<<(std::ostream& os, const TQueue<T>& queue) {
+    std::shared_ptr<TQueueItem<T>> item = queue.head;
 
-template <class T>
-std::shared_ptr<T> TQueue<T>::pop() {
-    std::shared_ptr<T> result;
-    if (first) {
-        result = first->GetItem();
-        first = first->GetNext();;
-    }
-    return result;
-}
-
-template <class T> 
-std::shared_ptr<T> TQueue<T>::top() {
-    if (first) {
-        return first->GetItem();
-    }
-}
-
-template <class T> 
-size_t TQueue<T>::size() { 
-    return n; 
-}
-
-template <class T> bool TQueue<T>::empty() { 
-    return first == nullptr; 
-}
-
-template <class T> std::ostream& operator<<(std::ostream& os, const TQueue<T>& q) {
-    std::shared_ptr<TQueueItem<T>> item = q.first;
-    while (item) {
-        os << *item;
+    while (item != nullptr) {
+        os << *item << " => ";
         item = item->GetNext();
     }
     return os;
 }
+
+template <class T>
+void TQueue<T>::Push(std::shared_ptr<T> &&square) {
+    std::shared_ptr<TQueueItem<T>> item = std::make_shared<TQueueItem<T>>(TQueueItem<T>(square));
+    if (item != nullptr) {
+        if (this->Empty()) {
+            this->head = this->tail = item;
+        }
+        else if (num_of_elem == 1) {
+            tail = item;
+            head->SetNext(item);
+        }
+        else {
+            this->tail->SetNext(item);
+            tail = item;
+        }
+        num_of_elem++;
+    }
+}
+
+template <class T>
+std::shared_ptr<T> TQueue<T>::Pop() {
+    std::shared_ptr<T> result;
+    if (head != nullptr) {
+        result = head->GetValue();
+        head = head->GetNext();
+        //item->SetNext(nullptr);
+        //delete item;
+    }
+    return result;
+}
+
+template <class T>
+std::shared_ptr<T> TQueue<T>::Top() {
+    if (head) {
+        return head->GetValue();
+    }
+}
+
+template <class T>
+bool TQueue<T>::Empty() {
+    return head == nullptr;
+}
+
+template <class T>
+size_t TQueue<T>::Length() {
+    return num_of_elem;
+}
+
+template <class T>
+TIterator<TQueueItem<T>, T> TQueue<T>::begin() {
+    return TIterator<TQueueItem<T>, T>(head);
+}
+
+template <class T>
+TIterator<TQueueItem<T>, T> TQueue<T>::end() {
+    return TIterator<TQueueItem<T>, T>(nullptr);
+}
+
+template <class T>
+TQueue<T>::~TQueue() {
+
+}
+
+#include "square.h"
+template class TQueue<Square>;
+template std::ostream& operator<<(std::ostream& os, const TQueue<Square>& queue);
