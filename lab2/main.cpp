3e676b01080d4aaca27de0729859965ab05b2550
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-12-01 12:14:17 +0300

    Update main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,11 +1,31 @@
 #include "tqueue.h"
 
-int main() {
-    TQueue* queue = new TQueue;
-    std::vector<Point> vect{ Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0) };
-    queue->push(Square(vect));
-    queue->push(Square());
-    std::cout << *queue << std::endl;
-    delete queue;
+int main(int argc, char** argv) {
+    TQueue queue;
+    Point a_1(1.0, 1.0);
+    Point b_1(1.0, 2.0);
+    Point c_1(2.0, 2.0);
+    Point d_1(2.0, 1.0);
+    Point a_2(3.0, 1.0);
+    Point b_2(3.0, 3.0);
+    Point c_2(5.0, 3.0);
+    Point d_2(5.0, 1.0);
+    Point a_3(0.0, 0.0);
+    Point b_3(0.0, 4.0);
+    Point c_3(4.0, 4.0);
+    Point d_3(4.0, 0.0);
+    queue.Push(Square(a_1, b_1, c_1, d_1));
+    queue.Push(Square(a_2, b_2, c_2, c_2));
+    queue.Push(Square(a_3, b_3, c_3, d_3));
+    std::cout << queue;
+
+    Square square;
+    square = queue.Pop();
+    std::cout << square;
+    square = queue.Pop();
+    std::cout << square;
+    square = queue.Pop();
+    std::cout << square;
+
     return 0;
 }
