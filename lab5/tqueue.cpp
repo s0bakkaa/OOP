520c10b8c78d760f87c2821725b39b89ad9afe1c
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-12-01 21:31:42 +0300

    Update tqueue.h

--- lab5/tqueue.cpp
+++ lab5/tqueue.cpp
@@ -1,89 +1,36 @@
-#include "tqueue.h"
+#ifndef TQUEUE_H
+#define TQUEUE_H
 
-template <class T>
-TQueue<T>::TQueue() : head(nullptr), tail(nullptr), num_of_elem(0) {
+#include "titerator.h"
+#include "tqueue_item.h"
 
-}
+template <class T> 
+class TQueue {
+public:
+    TQueue();
+    TQueue(const TQueue<T>& other);
 
-template <class T>
-TQueue<T>::TQueue(const TQueue<T>& other) {
-    head = other.head;
-}
+    void Push(std::shared_ptr<T> &&square);
+    std::shared_ptr<T> Pop();
 
-template <class T>
-std::ostream& operator<<(std::ostream& os, const TQueue<T>& queue) {
-    std::shared_ptr<TQueueItem<T>> item = queue.head;
+    std::shared_ptr<T> Top();
 
-    while (item != nullptr) {
-        os << *item << " => ";
-        item = item->GetNext();
-    }
-    return os;
-}
+    bool Empty();
 
-template <class T>
-void TQueue<T>::Push(std::shared_ptr<T> &&square) {
-    std::shared_ptr<TQueueItem<T>> item = std::make_shared<TQueueItem<T>>(TQueueItem<T>(square));
-    if (item != nullptr) {
-        if (this->Empty()) {
-            this->head = this->tail = item;
-        }
-        else if (num_of_elem == 1) {
-            tail = item;
-            head->SetNext(item);
-        }
-        else {
-            this->tail->SetNext(item);
-            tail = item;
-        }
-        num_of_elem++;
-    }
-}
+    size_t Length();
 
-template <class T>
-std::shared_ptr<T> TQueue<T>::Pop() {
-    std::shared_ptr<T> result;
-    if (head != nullptr) {
-        result = head->GetValue();
-        head = head->GetNext();
-        //item->SetNext(nullptr);
-        //delete item;
-    }
-    return result;
-}
+    template <class A>
+    friend std::ostream& operator<<(std::ostream& os, const TQueue<A>& queue);
 
-template <class T>
-std::shared_ptr<T> TQueue<T>::Top() {
-    if (head) {
-        return head->GetValue();
-    }
-}
+    TIterator<TQueueItem<T>, T> begin();
+    TIterator<TQueueItem<T>, T> end();
 
-template <class T>
-bool TQueue<T>::Empty() {
-    return head == nullptr;
-}
+    virtual ~TQueue();
 
-template <class T>
-size_t TQueue<T>::Length() {
-    return num_of_elem;
-}
+private:
+    std::shared_ptr<TQueueItem<T>> head;
+    std::shared_ptr<TQueueItem<T>> tail;
+    size_t num_of_elem;
+};
 
-template <class T>
-TIterator<TQueueItem<T>, T> TQueue<T>::begin() {
-    return TIterator<TQueueItem<T>, T>(head);
-}
-
-template <class T>
-TIterator<TQueueItem<T>, T> TQueue<T>::end() {
-    return TIterator<TQueueItem<T>, T>(nullptr);
-}
-
-template <class T>
-TQueue<T>::~TQueue() {
-
-}
-
-#include "square.h"
-template class TQueue<Square>;
-template std::ostream& operator<<(std::ostream& os, const TQueue<Square>& queue);
+#endif // TQUEUE_H
