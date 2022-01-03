f68bef8c64c9cc74e582718a4a6864306cce0c50
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2022-01-04 01:32:45 +0300

    Update lab02.cpp

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -1,101 +1,168 @@
 #include <iostream>
-
-typedef unsigned int ui;
-typedef unsigned long long ll;
+#include <string>
 
 class BitString {
 public:
-    BitString() = default;
+	BitString(): high(0), low(0) {}
+	BitString(unsigned long long h, unsigned int l) : high(h), low(l) {}
+	BitString(const BitString& bs) : high(bs.high), low(bs.low) {}
+	
+	unsigned int count() const;
 
-    BitString(BitString& cp) = default;
+	BitString& operator= (const BitString&);
+	BitString operator~ ();
 
-    BitString(ui fir, ll sec) : first(fir), second(sec) {};
+	friend BitString operator& (const BitString&, const BitString&);
+	friend BitString operator| (const BitString&, const BitString&);
+	friend BitString operator^ (const BitString&, const BitString&);
+	friend BitString operator<< (const BitString&, const unsigned int);
+	friend BitString operator>> (const BitString&, const unsigned int);
+	friend bool operator< (const BitString&, const BitString&);
+	friend bool operator> (const BitString&, const BitString&);
+	friend bool operator== (const BitString&, const BitString&);
+	friend bool operator!= (const BitString&, const BitString&);
 
-    BitString operator&(const BitString& right) const {
-        return { this->first & right.first, this->second & right.second };
-    }
+	friend std::ostream& operator<< (std::ostream&, const BitString&);
+	friend std::istream& operator>> (std::istream&, BitString&);
 
-    BitString operator|(const BitString& right) const {
-        return { this->first | right.first, this->second | right.second };
-    }
-
-    BitString operator^(const BitString& right) const {
-        return { this->first ^ right.first, this->second ^ right.second };
-    }
-
-    BitString operator!() const {
-        return { !this->first, !this->second };
-    }
-
-    BitString operator<<(int x) const {
-        if (x >= 64) {
-            return { second << (x - 64), 0 };
-        }
-        return { (first << x) | (second >> (64 - x)), second << x };
-    }
-
-    BitString operator>>(int x) const {
-        if (x >= 64) {
-            return { 0, first >> (x - 64) };
-        }
-        return { first >> x, (first << (64 - x)) | (second << x) };
-    }
-
-    int num_single_bits() {
-        int cnt = 0;
-        BitString cp(*this);
-        for (; cp.first; cp.first << 1, cp.second << 1) {
-            cnt += (int)cp.first & 1;
-            cnt += (int)cp.second & 1;
-        }
-        return cnt;
-    }
-    std::ostream& operator<<(std::ostream& os) {
-        os << this->first << "." << this->second;
-    }
-    bool operator<(BitString& rhs) {
-        return this->num_single_bits() < rhs.num_single_bits();
-    }
-    bool operator>(BitString& rhs) {
-        return this->num_single_bits() > rhs.num_single_bits();
-    }
-    bool substring(BitString& substr) const {
-        BitString cp = *this & substr;
-        return (cp.first != 0) && (cp.second != 0);
-    }
-    ui first_getter() {
-        return this->first;
-    }
-    ll second_getter() {
-        return this->second;
-    }
-    void first_setter(ll x) {
-        first = x;
-    }
-    void second_setter(ll x) {
-        second = x;
-    }
 private:
-    ui first;
-    ll second;
+	unsigned long long high;
+	unsigned int low;
 };
 
-unsigned long long operator""_division_by_2(unsigned long long x) {
-    return x / 2;
+unsigned int BitString::count() const {
+	unsigned int count = 0;
+	unsigned int l = low;
+	while (l != 0) {
+		count += l & 1;
+		l >>= 1;
+	}
+	unsigned long long h = high;
+	while (h != 0) {
+		count += h & 1;
+		h >>= 1;
+	}
+	return count;
 }
+
+BitString& BitString::operator= (const BitString& bs){
+	if (this == &bs) {
+			return *this;
+	}
+	high = bs.high;
+	low = bs.low;
+	return *this;
+}
+
+BitString BitString::operator~ () {
+	return BitString(~high, ~low);
+}
+
+BitString operator& (const BitString& lhs, const BitString& rhs) {
+	return BitString(lhs.high & rhs.high, lhs.low & rhs.low);
+}
+
+BitString operator| (const BitString& lhs, const BitString& rhs) {
+	return BitString(lhs.high | rhs.high, lhs.low | rhs.low);
+}
+
+BitString operator^ (const BitString& lhs, const BitString& rhs) {
+	return BitString(lhs.high ^ rhs.high, lhs.low ^ rhs.low);
+}
+
+BitString operator<< (const BitString& bs, unsigned int count) {
+	unsigned int low = (unsigned long long)bs.low << count;
+	unsigned long long high = bs.high << count;
+	if (count < 32) {
+		high |= bs.low >> (32 - count);
+	} else {
+		high |= (unsigned long long)bs.low << (count - 32);
+	}
+	return BitString(high, low);
+}
+
+BitString operator>> (const BitString& bs, unsigned int count) {
+	unsigned int low = (unsigned long long)bs.low >> count;
+	unsigned long long high = bs.high >> count;
+	if (count < 32) {
+		low |= bs.high << (32 - count);
+	} else {
+		low |= bs.high >> (count - 32);
+	}
+	return BitString(high, low);
+}
+
+bool operator< (const BitString& lhs, const BitString& rhs) {
+	return lhs.count() < rhs.count();
+}
+
+bool operator> (const BitString& lhs, const BitString& rhs) {
+	return lhs.count() > rhs.count();
+}
+
+bool operator== (const BitString& lhs, const BitString& rhs) {
+	return lhs.count() == rhs.count();
+}
+
+bool operator!= (const BitString& lhs, const BitString& rhs) {
+	return lhs.count() != rhs.count();
+}
+
+// user literal
+BitString operator"" _bs(const char* str, size_t len) {
+	if (len <= 8) {
+		return BitString(0, (unsigned int)strtoul(str, nullptr, 16));
+	} else {
+		return BitString(strtoul(str + 9, nullptr, 16), (unsigned int)strtoul(str, nullptr, 16));
+	}
+}
+
+std::ostream& operator<< (std::ostream& os, const BitString& bs) {
+	char str[25];
+	if (bs.high != 0) {
+		sprintf(str, "%llx%08x", bs.high, bs.low);
+	} else {
+		sprintf(str, "%x", bs.low);
+	}
+	os << str;
+	return os;
+}
+
+std::istream& operator>> (std::istream& is, BitString& bs) {
+	std::string str;
+	str.reserve(24);
+	is >> str;
+	if (str.size() <= 8) {
+		bs.high = 0;
+		bs.low = (unsigned int)stoul(str, nullptr, 16);
+	} else {
+		bs.high = stoull(str.substr(0, str.size()-8), nullptr, 16);
+		bs.low = stoull(str.substr(str.size()-8), nullptr, 16);
+	}
+	return is;
+}
+
 int main() {
-    BitString a, b;
-    std::cout << 4_division_by_2 << std::endl;
-    std::cout << "Enter first 128-BitString:" << std::endl << "> ";
-    ll first, second;
-    std::cin >> first >> second;
-    a.first_setter(first); 
-    a.second_setter(second);
-    std::cout << "Enter second 128-BitString:" << std::endl << "> ";
-    std::cin >> first >> second;
-    b.first_setter(first); 
-    b.second_setter(second);
-    BitString ans = a & b;
-    std::cout << "A & B:" << ans.first_getter() << "." << ans.second_getter();
-    return 0;
+	BitString a, b;
+	int n1, n2;
+	std::cout << "a = "; std::cin >> a;
+	std::cout << "b = "; std::cin >> b;
+	std::cout << "n1 = "; std::cin >> n1;
+	std::cout << "n2 = "; std::cin >> n2;
+
+	std::cout << "a | b = " << (a | b) << std::endl;
+	std::cout << "a & b = " << (a & b) << std::endl;
+	std::cout << "a ^ b = " << (a ^ b) << std::endl;
+	std::cout << "~a = " << ~a << std::endl;
+	std::cout << "a << n1 = " << (a << n1) << std::endl;
+	std::cout << "b >> n2 = " << (b >> n2) << std::endl;
+	std::cout << "(a << 48) | (b >> 48) = " << ((a << 48) | (b >> 48)) << std::endl;
+
+	std::cout << "count(a) = " << a.count() << std::endl;
+	std::cout << "count(b) = " << b.count() << std::endl;
+
+	std::cout << "a == b = " << (a == b) << std::endl;
+	std::cout << "a & ffff != b & ffff0000 = " << ((a & "ffff"_bs) != (b & "f0f0f0f0"_bs)) << std::endl;
+	std::cout << "a > 111111111111 = " << (a > "111111111111"_bs) << std::endl;
+	std::cout << "b < ffff0000 = " << (b < "ffff0000"_bs) << std::endl;
 }
